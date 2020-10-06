363b1680853410cdcfaa6fec90b4690633cd9e37
Anthony Ruiz <yobakaj@gmail.com>
2020-10-06 15:33:40 -0400

    added the Loading component into the principal account screen

--- app/screens/Account/Account.js
+++ app/screens/Account/Account.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { View, Text } from "react-native";
 import * as firebase from "firebase";
+import Loading from "../../components/Loading";
 import UserGuest from "./UserGuest";
 import UserLogged from "./UserLogged";
 
@@ -14,7 +14,7 @@ export default function Account() {
         });
     }, [])
 
-    if(login === null) return <Text>Cargando...</Text>
+    if(login === null) return <Loading isVisible={true} text="Cargando"/>
     
 
 
