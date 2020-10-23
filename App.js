34af212f945f654e26126d74d05f925711e816ea
Anthony Ruiz <yobakaj@gmail.com>
2020-10-23 13:06:13 -0400

    upload image to Firebase correctly.

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Navigation from "./app/navigations/Navigation";
 import {firebaseApp} from "./app/utils/firebase";
+import Navigation from "./app/navigations/Navigation";
 
 import { LogBox } from 'react-native'
 
 LogBox.ignoreAllLogs([
   'Animated: `useNativeDriver` was not specified. This is a required option and must be explicitly set to `true` or `false`',
+  'Setting a timer'
 ])
 
 export default function App() {
--- app/components/Account/InfoUser.js
+++ app/components/Account/InfoUser.js
@@ -7,25 +7,40 @@ import * as ImagePicker from "expo-image-picker";
 
 export default function InfoUser(props) {
   const { 
-    userInfo: { photoURL, displayName, email },
+    userInfo: { uid, photoURL, displayName, email },
     toastRef
-   } = props;
+  } = props;
 
-   const changeAvatar = async () => {
-     const resultPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-     const resultPermissionCamera = resultPermission.permissions.cameraRoll.status;
-     
-    if (resultPermission === "denied") {
+  const changeAvatar = async () => {
+  const resultPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+  const resultPermissionCamera = resultPermission.permissions.cameraRoll.status;
+
+    if (resultPermissionCamera === "denied") {
       toastRef.current.show("Es necesario aceptar los permisos de la galeria.");
     }else{
       const result = await ImagePicker.launchImageLibraryAsync({
         allowsEditing: true,
         aspect: [4, 3]
       });
-      console.log(result);
-    }
 
-   };
+      if(result.cancelled) {
+        toastRef.current.show("has Cerrado la seleccion de imagenes");
+      }else{
+        upLoadImage(result.uri).then(() => {
+          console.log("imagen subida");
+        }).catch(() => {
+          toastRef.current.show("Error actualizando el avatar.");
+        })
+      }
+    }
+  };
+
+  const upLoadImage = async (uri) => {
+    const response = await fetch(uri);
+    const blob = await response.blob();
+    const ref = firebase.storage().ref().child(`avatar/${uid}`);
+    return ref.put(blob);
+  };
 
   return (
     <View style={styles.viewUserInfo}>
