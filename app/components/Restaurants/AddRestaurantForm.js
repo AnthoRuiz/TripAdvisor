ded36a634069398d1fb2af0b41b27ded2adfa5e4
Anthony Ruiz <yobakaj@gmail.com>
2020-11-05 08:17:40 -0500

    added permissions to get Location and created the state to save that location.

--- app/components/Restaurants/AddRestaurantForm.js
+++ app/components/Restaurants/AddRestaurantForm.js
@@ -1,6 +1,13 @@
-import React, { useState } from "react";
-import { View, ScrollView, StyleSheet, Alert, Dimensions } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, ScrollView, StyleSheet, Alert, Dimensions, Text } from "react-native";
 import { Icon, Avatar, Image, Input, Button } from "react-native-elements";
+import * as Permissions from "expo-permissions";
+import * as ImagePicker from "expo-image-picker";
+import { map, size, filter } from "lodash";
+import * as Location from "expo-location";
+import Modal from "../Modal"
+
+const widthScreen = Dimensions.get("window").width;
 
 export default function AddRestaurantForm(props) {
 
@@ -8,6 +15,10 @@ export default function AddRestaurantForm(props) {
   const [restaurantName, setRestaurantName] = useState("");
   const [restaurantAddress, setRestaurantAddress] = useState("");
   const [restaurantDescription, setRestaurantDescription] = useState("");
+  const [imageSelected, setImageSelected] = useState([]);
+  const [isVisibleMap, setIsVisibleMap] = useState(false);
+
+  //console.log(imageSelected);
   
   const addRestaurant = () => {
     console.log("ok");
@@ -18,23 +29,42 @@ export default function AddRestaurantForm(props) {
   
   return (
     <ScrollView style={styles.scrollView}>
+      <ImageRestaurant imageRestaurant={imageSelected[0]} />
       <FormAdd 
         setRestaurantName={setRestaurantName}
         setRestaurantAddress={setRestaurantAddress}
         setRestaurantDescription={setRestaurantDescription}
+        setIsVisibleMap={setIsVisibleMap}
       />
-      <UploadImage />
-        <Button 
-          title="Crear Restaurante"
-          onPress={addRestaurant}
-          buttonStyle={styles.btnAddRestaurant}
-        />
+      <UploadImage 
+        toastRef={toastRef} 
+        imageSelected={imageSelected}
+        setImageSelected={setImageSelected}
+      />
+      <Button 
+        title="Crear Restaurante"
+        onPress={addRestaurant}
+        buttonStyle={styles.btnAddRestaurant}
+      />
+      <Map isVisibleMap={isVisibleMap} setIsVisibleMap={setIsVisibleMap}/>
     </ScrollView>
   );
 }
 
+function ImageRestaurant(props) {
+  const {imageRestaurant} = props;
+  return(
+    <View style={styles.viewPhoto}>
+      <Image 
+        source={imageRestaurant ? {uri:imageRestaurant} : require("../../../assets/img/no-image.png")}
+        style={{ width:widthScreen, height:200 }}
+      />
+    </View>
+  );
+}
+
 function FormAdd(props){
-  const {setRestaurantName, setRestaurantAddress, setRestaurantDescription} = props;
+  const {setRestaurantName, setRestaurantAddress, setRestaurantDescription, setIsVisibleMap} = props;
   return (
     <View style={styles.viewForm}>
       <Input
@@ -46,6 +76,12 @@ function FormAdd(props){
         placeholder="Direccion" 
         containerStyle={styles.input}
         onChange={e=> setRestaurantAddress(e.nativeEvent.text)}
+        rightIcon={{
+          type: "material-community",
+          name: "google-maps",
+          color: "#c2c2c2",
+          onPress: () => setIsVisibleMap(true)
+        }}
       />
       <Input
         placeholder="Descripcion del Restaurante"
@@ -57,14 +93,88 @@ function FormAdd(props){
   );
 }
 
-function UploadImage() {
+function Map(props) {
+  const { isVisibleMap, setIsVisibleMap } = props;
+  const [location, setLocation] = useState(null);
+  useEffect(() => {
+    (async() => {
+      const resultPermissions = await Permissions.askAsync(
+        Permissions.LOCATION
+      );
+      const statusPermissions = resultPermissions.permissions.location.status;
 
-  const imageSelect = () => {
-    console.log("imagenes");
+      if(statusPermissions !== "granted"){
+        toastRef.current.show("Tienes que aceptar los permisos de localizacion para crear un restaurante", 3000);
+      }else{
+        const loc = await Location.getCurrentPositionAsync({});
+        console.log(loc);
+        setLocation({
+          latitude: loc.coords.latitude,
+          longitude: loc.coords.longitude,
+          latitudeDelta: 0.001,
+          longitudeDelta: 0.001
+        })
+      }
+
+    })()
+  }, [])
+
+  return(
+    <Modal isVisible={isVisibleMap} setIsVisible={setIsVisibleMap}>
+      <Text>Mapa</Text>
+    </Modal>
+  );
+}
+
+function UploadImage(props) {
+  const { toastRef, setImageSelected, imageSelected } = props;
+
+  const imageSelect = async () => {
+    const resultPermissions = await Permissions.askAsync(
+      Permissions.CAMERA_ROLL
+    );
+    if(resultPermissions === "denied"){
+      toastRef.current.show("Es necesario los permisos de la galeria, si los has rechazado deber ir a ajustes y configurarlo manualment", 3000);
+    }else{
+      const result = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        aspect: [4,3]
+      });
+      
+      if(result.cancelled){
+        toastRef.current.show("Has cerrado la galeria sin seleccionar ninguna imagen", 2000);
+      }else{
+        setImageSelected([...imageSelected, result.uri])
+      }
+    }
   };
+
+  const removeImage = (image) => {
+
+    Alert.alert(
+      "Eliminar Imagen",
+      "Estas seguro de que queires eliminar la imagen?",
+      [
+        {
+          text: "Cancel",
+          style: "cancel"
+        },
+        {
+          text: "Eliminar",
+          onPress: () => {
+            setImageSelected(filter(imageSelected, (imageUrl) => imageUrl !== image)) 
+          }
+        }
+      ],
+      { cancelable: false }
+    )
+
+  }
   
   return(
     <View style={styles.viewImages}>
+    {size(imageSelected) < 4 && 
+    (
       <Icon 
         type="material-community"
         name="camera"
@@ -72,6 +182,19 @@ function UploadImage() {
         containerStyle={styles.containerIcon}
         onPress={imageSelect}
       />
+    )
+    }
+      
+      {
+        map(imageSelected, (imageRestaurant, index) => (
+          <Avatar 
+            key={index}
+            style={styles.miniatureStyle}
+            source={{uri: imageRestaurant}}
+            onPress={() => removeImage(imageRestaurant)}
+          />
+        ))
+      }
 
     </View>
   )
